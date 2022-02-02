3d1e872e942ab081c97cc9e681f3f011021b9205
Musab1258 <67251181+Musab1258@users.noreply.github.com>
2022-02-03 00:01:42 +0100

    Create Carousel.js

---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,37 @@
+const Carousel = () => {
+  return (
+    <div>
+      
+      <Header>
+      </Header>
+      
+      <main>
+      <section>
+      <p>Do you know that: In Lagos alone, about 9,000 tonnes of waste are generated daily with about 86 per cent of it plastic.</p>
+      </section>
+      
+      <section>
+      <p>Due to poor waste management awareness residents dump waste indiscriminately into drainage channels and on the streets.</p>
+      </section>
+      
+      <section>
+      <p>These dumped waste leads to environmental degradation and also, flooding.</p>
+      </section>
+      
+      <section>
+      <p>At Recycle, we recycle plastic waste, but, we need you to help us gather them while we pay you.</p>
+      </section>
+      
+      <section>
+      <p>You can click the button below to Check out our rates and also calculate the amount you will be paid for the quantity of waste you bring.</p>
+      </section>
+      </main>
+      
+      <footer>
+      </footer>
+      
+    </div>
+  )
+}
+
+export default Carousel;
