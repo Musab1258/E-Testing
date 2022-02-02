1dd43eaf566d2ace3b95c4733aeb48d89996b2cb
Musab1258 <67251181+Musab1258@users.noreply.github.com>
2022-02-03 00:07:48 +0100

    Create RewardCalculator.js

---
+++ src/components/RewardCalculator/RewardCalculator.js
@@ -0,0 +1,11 @@
+const RewardCalculator = () => {
+  return (
+    <form>
+      <input type='num' />
+      <button placeholder='Submit' />
+    </form>
+    <p></p>
+  )
+}
+
+export default RewardCalculator;
