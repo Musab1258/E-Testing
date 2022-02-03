f11989ae600996ea319303873e83e5526128354c
Musab1258 <67251181+Musab1258@users.noreply.github.com>
2022-02-03 01:12:39 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import RewardCalculator from './components/RewardCalculator/RewardCalculator';
 const App = () => {
   return (
     <div>
+      <h1>Render</h1>
+      <p>Why are you not rendering</p>
       <Carousel />
       <RewardCalculator />
     </div>
