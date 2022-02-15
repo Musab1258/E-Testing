2e95f484b8036fc4757fc0ca115a967c3a07df13
Musab1258 <67251181+Musab1258@users.noreply.github.com>
2022-02-15 03:41:50 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
-import Carousel from './components/Carousel/Carousel';
-import RewardCalculator from './components/RewardCalculator/RewardCalculator';
+import Carousel from './components/Carousel'
 
-
-const App = () => {
-  return (
-    <div>
-      <h1>Render</h1>
-      <p>Why are you not rendering</p>
-      <Carousel />
-      <RewardCalculator />
-    </div>
-  );
+function App() {
+  return <Carousel />
 }
 
-export default App;
+export default App
+
