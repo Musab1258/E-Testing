776944d0fbdab74e7c3b19eb95becb60e62e1abc
Musab1258 <67251181+Musab1258@users.noreply.github.com>
2022-02-16 16:01:30 +0100

    Update Carousel.js

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -31,20 +31,20 @@ export default function Carousel() {
 
   const facts = [
     {
-      imageSrc: "../images/Waste.jpeg.jpg"
-      description: "Do you know that: In Lagos alone, about 9,000 tonnes of waste are generated daily with about 86 per cent of it plastic."
+      imageSrc: "../images/Waste.jpeg.jpg",
+      description: "Do you know that:  In Lagos alone, about 9,000 tonnes of waste are generated daily with about 86 per cent of it plastic.",
     },
     {
-      imageSrc: "../images/Drainage.jpeg.jpg"
-      description: "Due to poor waste management awareness residents dump waste indiscriminately into drainage channels and on the streets."
+      imageSrc: "../images/Drainage.jpeg.jpg",
+      description: "Due to poor waste management awareness residents dump waste indiscriminately into drainage channels and on the streets.",
     },
     {
-      imageSrc: "../images/Flooding.jpeg.jpg"
-      description: "These dumped waste leads to environmental degradation and also, flooding."
+      imageSrc: "../images/Flooding.jpeg.jpg",
+      description: "These dumped waste leads to environmental degradation and also, flooding.",
     },
     {
-      imageSrc: "../images/Cash.jpeg.jpg"
-      description: "At Recycle, we recycle plastic waste, but, we need you to help us gather them while we pay you."
+      imageSrc: "../images/Cash.jpeg.jpg",
+      description: "At Recycle, we recycle plastic waste, but, we need you to help us gather them while we pay you.",
     },
   ]
 
