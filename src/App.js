e75f462102cbdb5de4b0ed62b583e5a0bf2116b8
Musab1258 <habeebmusab@gmail.com>
2022-08-26 22:30:30 +0000

    update

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ function App() {
     <div className="border-2 w-[80rem] min-h-screen shadow-2xl flex">
       <img src={rectangle} alt="Top rectangle" className="h-80"/>
       <div className="w-full flex flex-col justify-center items-center text-center -ml-24 -mt-10" >
-        <img src={perxels} alt="Perxels logo" className="w-28 mb-5"/>
-        <img src={youngman} alt="young man working" className="h-[40%] border-2 border-yellow mb-6 py-2"/>
-        <h1 className="text-3xl mb-4 w-2/6 text-blue">Welcome to Perxel's e-assesment platform</h1>
-        <p className="text-lg text-center mb-4 w-2/5">We wish you a blissful experience and great luck in your assesment</p>
-        <Link to="/Signin" className="text-xl border py-2 w-64 bg-blue text-white rounded-md"><button>Get Started</button></Link>
-        <p className="text-sm text-center">Designed by <a href="https://ng.linkedin.com/in/ibrahim-salami-515319168">Ibrahim Salami</a></p>
+        <img src={perxels} alt="Perxels logo" className="w-28 mb-4"/>
+        <img src={youngman} alt="young man working" className="h-[40%] border-2 border-yellow mb-5 py-2"/>
+        <h1 className="text-3xl mb-3 w-2/6 text-blue">Welcome to Perxel's e-assesment platform</h1>
+        <p className="text-lg text-center mb-3 w-2/5">We wish you a blissful experience and great luck in your assesment</p>
+        <Link to="/Signin" className="text-xl border py-2 w-64 bg-blue mb-1 text-white rounded-md"><button>Get Started</button></Link>
+        <p className="text-sm text-center">Designed by: <a href="https://ng.linkedin.com/in/ibrahim-salami-515319168" className="underline text-blue">Ibrahim Salami</a></p>
       </div>
     </div>
   );
